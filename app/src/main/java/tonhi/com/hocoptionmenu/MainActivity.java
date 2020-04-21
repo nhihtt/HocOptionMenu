764b08ca01b1c92e1ac28f924e8fc8f908cafdf8
HoThiToNhi <nhihtt18406@st.uel.edu.vn>
2020-04-21 12:27:07 +0700

    Initial commit

---
+++ app/src/main/java/tonhi/com/hocoptionmenu/MainActivity.java
@@ -0,0 +1,41 @@
+package tonhi.com.hocoptionmenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.mnuHuongDan:
+                Intent intent =new Intent(MainActivity.this,HuongDanActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.mnuGioiThieu:
+                intent =new Intent(MainActivity.this,GioiThieuActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
